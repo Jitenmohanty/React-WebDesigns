f3a235dfb2660053187a3737ef7bcf0612588179
Jiten Kumar Mohanty <105143946+Jitenmohanty@users.noreply.github.com>
2024-03-04 14:26:32 +0530

    Delete ContextApi3 directory

--- ContextApi3/src/components/ClearAll.jsx
+++
@@ -1,9 +0,0 @@
-const ClearAll = ({ clearAllTodo }) => {
-  return (
-    <div className="float-right bg-red-500 text-white p-2 mt-4 mr-4 md:mr-96  rounded-md">
-      <button onClick={clearAllTodo}>Clear All</button>
-    </div>
-  );
-};
-
-export default ClearAll;
